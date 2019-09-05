1782ce3f79e80ab18322a26275f99b4a3fc275ef
vanfigo <rodrigo.loyola@live.com>
2019-09-05 12:42:46 -0500

    config server setup

--- src/main/java/com/bucketdev/betappconfigserver/BetappConfigServerApplication.java
+++ src/main/java/com/bucketdev/betappconfigserver/BetappConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.bucketdev.betappconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BetappConfigServerApplication {
 
 	public static void main(String[] args) {
