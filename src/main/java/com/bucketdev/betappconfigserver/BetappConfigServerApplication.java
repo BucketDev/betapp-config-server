e94b5123127603f4288c98b6f53b257af5ffbeef
vanfigo <rodrigo.loyola@live.com>
2019-09-04 20:06:28 -0500

    spring config server first commit

---
+++ src/main/java/com/bucketdev/betappconfigserver/BetappConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betappconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappConfigServerApplication.class, args);
+	}
+
+}
